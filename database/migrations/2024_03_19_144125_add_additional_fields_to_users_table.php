0ee25ecd70dfd94561002c870ca2ca1096cd7e2d
Usama <usamamajeed414@gmail.com>
2024-03-19 19:47:45 +0500

    Add additional field to user table for applying for job

---
+++ database/migrations/2024_03_19_144125_add_additional_fields_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('job_title', 200)->default('No job title')->after('password');
+            $table->string('bio', 200)->default('No bio')->after('job_title');
+            $table->string('twitter', 200)->default('No twitter')->after('bio');
+            $table->string('facebook', 200)->default('No facebook')->after('twitter');
+            $table->string('linkedin', 200)->default('No linkedin')->after('facebook');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('job_title');
+            $table->dropColumn('bio');
+            $table->dropColumn('twitter');
+            $table->dropColumn('facebook');
+            $table->dropColumn('linkedin');
+        });
+    }
+};
