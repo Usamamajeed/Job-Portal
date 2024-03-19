176a20c64c2addbfa75602b3e5237ffd37c582b8
Usama <usamamajeed414@gmail.com>
2024-03-19 19:06:44 +0500

    Hide Jobs values for save job

--- resources/views/jobs/single.blade.php
+++ resources/views/jobs/single.blade.php
@@ -71,13 +71,13 @@
                             <div class="col-6">
                                 <form action="{{ route('save.job') }}" method="POST">
                                     @csrf
-                                    <input name="job_id" type="text" value="{{ $job->id }}"/>
-                                    <input name="user_id" type="text" value="{{ Auth::user()->id }}"/>
-                                    <input name="job_image" type="text" value="{{ $job->image }}"/>
-                                    <input name="job_title" type="text" value="{{ $job->job_title }}"/>
-                                    <input name="job_region" type="text" value="{{ $job->job_region }}"/>
-                                    <input name="job_type" type="text" value="{{ $job->job_type }}"/>
-                                    <input name="company" type="text" value="{{ $job->company }}"/>
+                                    <input name="job_id" type="hidden" value="{{ $job->id }}"/>
+                                    <input name="user_id" type="hidden" value="{{ Auth::user()->id }}"/>
+                                    <input name="job_image" type="hidden" value="{{ $job->image }}"/>
+                                    <input name="job_title" type="hidden" value="{{ $job->job_title }}"/>
+                                    <input name="job_region" type="hidden" value="{{ $job->job_region }}"/>
+                                    <input name="job_type" type="hidden" value="{{ $job->job_type }}"/>
+                                    <input name="company" type="hidden" value="{{ $job->company }}"/>
                                     <button name="submit" type="submit" class="btn btn-block btn-light btn-md"><i class="icon-heart"></i>Save Job</button>
                                 </form>
                                 <!--add text-danger to it to make it read-->
