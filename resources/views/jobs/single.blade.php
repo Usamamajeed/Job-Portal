4745541c3aa104d18ccda2982839c5a4cb6d0060
Usama <usamamajeed414@gmail.com>
2024-03-20 19:23:31 +0500

    Show Jobs In Category When click on category

--- resources/views/jobs/single.blade.php
+++ resources/views/jobs/single.blade.php
@@ -158,7 +158,7 @@
                             <h3 class="text-primary  mt-3 h5 pl-3 mb-3 ">Categories</h3>
                             <ul class="list-unstyled pl-3 mb-0">
                                 @foreach($categories as $category)
-                                    <li class="mb-2"><a href="#"> {{$category->name}} </a></li>
+                                    <li class="mb-2"><a class="text-decoration-none" href="{{ route('categories.single', $category->name) }}"> {{$category->name}} </a></li>
                                 @endforeach
                             </ul>
                         </div>
